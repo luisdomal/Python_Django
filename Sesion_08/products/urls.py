afb414e2c92e44dd8b4e96e6b379ec067b75e8f9
Luis Dominguez <l.dominguez.aldana@gmail.com>
2021-08-31 21:31:53 -0500

    Sesion 08 updated

--- Sesion_08/products/urls.py
+++ Sesion_08/products/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("", views.ProductListView.as_view(), name="list"),  # products:list
     path("new/", views.ProductCreateView.as_view(), name="create"),  # products:create
     path("checkout/<int:pk>", views.checkout, name="checkout"),  # products:checkout
+    path("editor/", views.permisos_v2, name="permisos"),  # products:checkout
 ]
--- Sesion_08/products/views.py
+++ Sesion_08/products/views.py
@@ -1,5 +1,7 @@
 """Products app views"""
 
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView
@@ -27,3 +29,21 @@ def checkout(request, pk):
     product = get_object_or_404(Product, pk=pk)
     checkout_price = stripe.checkout(product.price)
     return render(request, "products/checkout.html", {"checkout_price": checkout_price})
+
+
+def permisos(request):
+    if request.user.groups.filter(name="Editor").exists():
+        return HttpResponse("Hola, editor")
+    else:
+        # 401 Unauthorized - Autenticación
+        # 403 Forbidden - Autorización
+        return HttpResponse("Acceso denegado", status=403)
+
+
+def is_editor(user):
+    return user.groups.filter(name="Editor").exists()
+
+
+@user_passes_test(is_editor)
+def permisos_v2(request):
+    return HttpResponse("Hola, editor")
